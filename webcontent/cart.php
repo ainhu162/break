fc105a5798f019aa636ebcc87fcbff1b6767c317
Nhu <tuainhu160296@gmail.com>
2017-06-12 14:34:50 +0700

    a

--- webcontent/cart.php
+++ webcontent/cart.php
@@ -6,7 +6,7 @@ $connect = mysqli_connect("localhost", "root", "", "shopping_cart");
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>wbl Multi Tab Shopping Cart By Using PHP Ajax Jquery Bootstrap Mysql</title>
+		<title>wbl adsMulti Tab Shopping Cart By Using PHP Ajax Jquery Bootstrap Mysql</title>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
